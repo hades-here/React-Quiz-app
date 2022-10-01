52b9eee96d7972f6825971bad7c04b6fde97bb85
hades-here <punithprachi113@gmail.com>
2022-10-01 19:25:52 +0530

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,46 @@
+export const Questions = [
+{
+    prompt: "Which of these is not a type of African drum?",
+    optionA: "djembe",
+    optionB: "timpani",
+    optionC: "gudugudu",
+    optionD: "talking drum",
+    answer: "B",
+},
+
+{
+    prompt: "The theremin belongs to this class of musical instruments ?",
+    optionA: "electronic",
+    optionB: "wind",
+    optionC: "keyboard",
+    optionD: "percussion",
+    answer: "A",
+},
+
+{
+    prompt: "How many pedals does a grand piano have?",
+    optionA: "3",
+    optionB: "2",
+    optionC: "4",
+    optionD: "1",
+    answer: "A",
+},
+{
+    prompt: "The saxophone is a popular jazz instrument, but what was its first use?",
+    optionA: "military music",
+    optionB: "drowning out flutes",
+    optionC: "wall ornament",
+    optionD: "bluegrass music",
+    answer: "A",
+},
+{
+    prompt: "What is the word for a kind of stringed instrument?",
+    optionA: "harpy",
+    optionB: "harp",
+    optionC: "trombone",
+    optionD: "harmonica",
+    answer: "B",
+},
+
+
+];
